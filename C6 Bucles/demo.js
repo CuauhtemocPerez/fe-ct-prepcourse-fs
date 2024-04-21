eb7d4aff43ca00d0ca915957648a01d01d7adddd
Cuautemoc <Cuautemocperezarzate@gmail.com>
2024-04-21 13:46:15 -0600

    modificando C6

---
+++ C6 Bucles/demo.js
@@ -0,0 +1,13 @@
+var x=15
+var y=10
+
+if (x > y) {return "X"} 
+else { if (x = y) { 
+    const Resultado = obtenerMayor(x,y)
+    return Resultado
+    } else {
+      return "y"
+     }
+    }
+
+  
--- C6 Bucles/ejercicios/06.js
+++ C6 Bucles/ejercicios/06.js
@@ -2,6 +2,11 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+
+  var DigQty = num.length
+  
+  if (DigQty.length = 3) return true; else return false
+  
 }
 
 module.exports = tieneTresDigitos;
